d2669271cca9350b5c46f42268439fa4bfd0d83e
TrongHuy317 <huytrong124317@gmail.com>
2026-09-15 16:33:53 +0700

    TODO2.5: feat(dao): implement CRUD operations for DepartmentDAO and EmployeeDAO

---
+++ slot3/Chapter1-JPA-MappingOneToMany/src/main/java/fe/DE200248/dao/EmployeeDAO.java
@@ -0,0 +1,84 @@
+package fe.DE200248.dao;
+
+import fe.DE200248.pojo.Employee;
+import fe.DE200248.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+import java.util.List;
+
+public class EmployeeDAO {
+
+    public void save(Employee employee) {
+        EntityManager em = JPAUtil.getEMF().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            em.persist(employee);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    public Employee update(Employee employee) {
+        EntityManager em = JPAUtil.getEMF().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Employee merged = em.merge(employee);
+            tx.commit();
+            return merged;
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    public void delete(Long id) {
+        EntityManager em = JPAUtil.getEMF().createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        try {
+            tx.begin();
+            Employee employee = em.find(Employee.class, id);
+            if (employee != null) {
+                em.remove(employee);
+            }
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    public Employee findById(Long id) {
+        EntityManager em = JPAUtil.getEMF().createEntityManager();
+        try {
+            return em.find(Employee.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Employee> findAll() {
+        EntityManager em = JPAUtil.getEMF().createEntityManager();
+        try {
+            return em.createQuery("SELECT e FROM Employee e", Employee.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+}
