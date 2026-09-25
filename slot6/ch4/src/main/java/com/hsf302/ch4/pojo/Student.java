6e4c443cc98a0d6c4b98298f321144a6ce0b4c07
TrongHuy317 <huytrong124317@gmail.com>
2026-09-25 14:13:46 +0700

    feat(entity): add Gender enum and Department entity
    Department là inverse side với @OneToMany(mappedBy = department)
    
    Refs: TODO 2

---
+++ slot6/ch4/src/main/java/com/hsf302/ch4/pojo/Student.java
@@ -0,0 +1,28 @@
+package com.hsf302.ch4.pojo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Student {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
